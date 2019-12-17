9e29aff4af0e691f3442d327e7d454c7e216d2ad
grantfogle <grantjfogle@gmail.com>
2019-12-16 21:17:42 -0700

    adding pg and knex

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.listen(port, () => {
+app.use(bodyParser());
+app.use(cors());
+
+app.listen(port, (req, res) => {
     console.log(`listening on ${port}`);
+});
+app.get('/', (req, res) => {
+    return res.send('The route worked!!!');
 });
